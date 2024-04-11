d0cee157928e6b3e6e65d98c58096873f2b2ada0
SolDev-HP <develop.hp.block@gmail.com>
2024-04-11 18:08:02 -0400

    Start with RewardPoints campaign, basics

--- BadgeFactory_SmartContracts/contracts/RewardPoints.sol
+++ BadgeFactory_SmartContracts/contracts/RewardPoints.sol
@@ -10,11 +10,28 @@ pragma solidity 0.8.20;
 /// the owner will be assigned to msg.sender
 contract RewardPoints {
     // ------------- State Vars
-    address public campaign_owner;
+    address public campaign_owner_console;
+    uint256 private _total_points;
+    uint256 private _total_customers_counter;
+
+    mapping(uint256 => address) private _cust_id_to_cust_address;
+    mapping(address => bool) private _is_cust_subscribed;
+    mapping(address => uint256) private _points_of_customer;
+
+    // Probably need events if we need these data for analytics
+    event RewardPointsRedeemed(address _customer, uint256 _points);
+    event RewardPointsGained(address _customer, uint256 _points);
 
     // ------------- Modifier
     modifier onlyConsole() {
-        require(msg.sender == campaign_owner, "OnlyConsole!");
+        require(msg.sender == campaign_owner_console, "OnlyConsole!");
+        _;
+    }
+
+    // Testing only
+    modifier onlySelf(address customer) {
+        require(_is_cust_subscribed[customer], "UnSubbed");
+        require(msg.sender == customer, "OnlyYours!");
         _;
     }
 
@@ -33,14 +50,82 @@ contract RewardPoints {
     // redeem reward points
     // transfer reward points to friends, family
     constructor(address camp_owner) {
-        campaign_owner = camp_owner;
+        campaign_owner_console = camp_owner;
     }
 
-    function check_points() external view onlyConsole returns (uint) {
-        return 1;
+    function subscribe_customer(address customer) external onlyConsole {
+        // Assign custid->address
+        _cust_id_to_cust_address[_total_customers_counter] = customer;
+        // Increase total number
+        _total_customers_counter += 1;
+        // Customer has subscribed
+        _is_cust_subscribed[customer] = true;
+    }
+
+    function unsubscribe_customer(address customer) external onlyConsole {
+        // Weeeeell, you can't right now, not the focus of this POC
+        // _is_cust_subscribed[customer] = false;
+    }
+
+    function total_subscribers()
+        external
+        view
+        onlyConsole
+        returns (uint256 no_of_subs)
+    {
+        no_of_subs = _total_customers_counter;
+    }
+
+    function check_points(
+        address customer
+    ) external view onlyConsole returns (uint256 total_points) {
+        return _points_of_customer[customer];
+    }
+
+    function check_my_points(
+        address customer
+    ) external view onlySelf(customer) returns (uint256 total_points) {
+        return _points_of_customer[customer];
     }
 
     function check_owner() external view returns (address) {
-        return campaign_owner;
+        return campaign_owner_console;
+    }
+
+    function reward_points(
+        address customer,
+        uint256 _additional_points
+    ) external onlyConsole returns (uint256 customer_new_total) {
+        // Is this customer subscribed?
+        require(_is_cust_subscribed[customer], "NotSubbed");
+        // Reward customer with points
+        // Customer identifier required
+        unchecked {
+            _points_of_customer[customer] += _additional_points;
+            // Increase total circulating points count
+            _total_points += _additional_points;
+            // Returns customer's new points total
+            customer_new_total = _points_of_customer[customer];
+        }
+        emit RewardPointsGained(customer, _additional_points);
+    }
+
+    function redeem_points(
+        address customer,
+        uint256 _redeemed_points
+    ) external onlyConsole returns (uint256 customer_new_total) {
+        // Customer subscribed?
+        require(_is_cust_subscribed[customer], "NotSubbed");
+        // Customer redeems the points
+        require(
+            _points_of_customer[customer] >= _redeemed_points,
+            "NotEnoughPoints"
+        );
+        require(_total_points >= _redeemed_points, "CantHappen");
+        unchecked {
+            _points_of_customer[customer] -= _redeemed_points; // reduce customer's points, it can become 0
+            customer_new_total = _points_of_customer[customer]; // return new total balance
+        }
+        emit RewardPointsRedeemed(customer, _redeemed_points);
     }
 }
