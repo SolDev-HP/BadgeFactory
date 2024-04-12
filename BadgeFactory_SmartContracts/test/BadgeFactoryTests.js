54976bbb92a6a75bcf6235b75315de2e896fb326
SolDev-HP <develop.hp.block@gmail.com>
2024-04-12 00:19:21 -0400

    Start unittests, badgefactory and loyaltyconsole

--- BadgeFactory_SmartContracts/test/BadgeFactoryTests.js
+++ BadgeFactory_SmartContracts/test/BadgeFactoryTests.js
@@ -1,4 +1,28 @@
 // Tests for deploying badgefactory
 // multiple consoles simultaneously deployed
 // access control on loyaltyconsole and campaigns
-// 
+const { expect } = require("chai");
+const { hardhatArguments } = require("hardhat");
+require("ethers");
+require("hardhat");
+
+describe("BadgeFactory", function () {
+    it("BadgeFactory deployment and campaigns deployer deployment", async function () {
+        // Owner of the badgefactory
+        const [owner] = await ethers.getSigners();
+
+        // Deploy badgefactory, it also deploys campaign deployer
+        const badgeFactory_contract = await ethers.deployContract(
+            "BadgeFactory"
+        );
+        await badgeFactory_contract.waitForDeployment();
+        //console.log(badgeFactory_contract)
+
+        // await expect(badgeFactory_contract).to.emit(
+        //     await badgeFactory_contract.getAddress(),
+        //     "DeployerAt"
+        // );
+
+        //console.log(await badgeFactory_contract.getAddress());
+    });
+});
