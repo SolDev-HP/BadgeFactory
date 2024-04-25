b735abbc4c0df176cdf59fbc57c33f3fa45409bb
SolDev-HP <develop.hp.block@gmail.com>
2024-04-25 03:04:36 -0400

    Adjust pages, add contracts page, add empty pages with identifying titles

---
+++ badgefactory_frontend/src/app/contracts/page.tsx
@@ -0,0 +1,11 @@
+// To deploy the list of all possible
+// contracts deployed on all of the chains (verified contracts)
+
+export default function Contracts() {
+    return (
+      <div className="flex min-h-[88%] w-[100%] flex-col items-center justify-between mt-4 p-4 bg-bf-comp-bg rounded-xl">
+        <h3> BadgeFactory - Deployed Contracts List </h3>
+      </div>
+    );
+  }
+  
