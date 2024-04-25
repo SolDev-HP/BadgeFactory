2a6ed16c66ccddf0d1008c95db1583916ce4220b
SolDev-HP <develop.hp.block@gmail.com>
2024-04-25 03:35:48 -0400

    remove empty route

--- badgefactory_frontend/src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-//// Specifically for nextauth
-// once I start using diversity and their provider options
-/// I can create credentialProviders here and pass to nextauth
-// through prisma, add data onto postgres 
-/// credentialsProvider(google|github|phone|email|wallet) 
-//// - may be others- diversity has quite a few options
