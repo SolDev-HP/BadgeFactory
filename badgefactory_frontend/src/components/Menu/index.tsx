d69b34800d3b1ee3b5f2a102867346d797a8c93a
SolDev-HP <develop.hp.block@gmail.com>
2024-04-25 01:33:09 -0400

    Menu updates and fixes

--- badgefactory_frontend/src/components/Menu/index.tsx
+++ badgefactory_frontend/src/components/Menu/index.tsx
@@ -3,7 +3,8 @@
 // [ItemIcon] ItemName
 // ...
 // [SocialIcon] SocialLink
-
+'use client'
+import { usePathname } from "next/navigation";
 import MenuItem from "../MenuItem";
 
 /// There are two divisions
@@ -11,6 +12,8 @@ import MenuItem from "../MenuItem";
 //// SocialLink items
 
 export default function Menu({}) {
+    // Check which path is active and set that path active for style change
+    const pathname = usePathname();
   return (
     <>
       <div className="flex flex-col justify-between h-[90vh]">
@@ -20,32 +23,37 @@ export default function Menu({}) {
             menu_icon={"home_icon"}
             menu_link={"/"}
             menu_text={"Home"}
-            is_active={true}
+            is_active={pathname == "/" ? true : false}
           />
           <MenuItem
             menu_icon={"entity_icon"}
             menu_link={"/entity"}
             menu_text={"Entity Dashboard"}
+            is_active={pathname == "/entity" ? true : false}
           />
           <MenuItem
             menu_icon={"cust_icon"}
             menu_link={"/customer"}
             menu_text={"Customer Dashboard"}
+            is_active={pathname == "/customer" ? true : false}
           />
           <MenuItem
             menu_icon={"about_icon"}
             menu_link={"/about"}
             menu_text={"About BadgeFactory"}
+            is_active={pathname == "/about" ? true : false}
           />
           <MenuItem
             menu_icon={"contact_icon"}
             menu_link={"/contact"}
             menu_text={"Contact Us"}
+            is_active={pathname == "/contact" ? true : false}
           />
           <MenuItem
             menu_icon={"contact_icon"}
             menu_link={"/contracts"}
             menu_text={"Deployments"}
+            is_active={pathname == "/contracts" ? true : false}
           />
         </div>
         {/**  This is for social links*/}
